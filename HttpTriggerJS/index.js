33ad7c21ec082e752f39caaf4d6ac72a31945e05
Richard Spitz <rspitz@microsoft.com>
2018-12-31 14:08:11 -0800

    Fourth Commit

--- HttpTriggerJS/index.js
+++ HttpTriggerJS/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hey There, " + (req.query.name || req.body.name)
+            body: "How's it going, " + (req.query.name || req.body.name)
         };
     }
     else {
